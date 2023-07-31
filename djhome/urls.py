a41b7f5311117eed0660287ab63a95f7999ff60e
Guilherme H <ghpm99@gmail.com>
2023-07-31 01:14:14 -0300

    Feat: Implements static files

--- djhome/urls.py
+++ djhome/urls.py
@@ -1,9 +1,13 @@
-from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='home', permanent=True)),
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
