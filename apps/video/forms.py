aee9aa4fdfb53834e2c905dac06afb5d1abbb7ea
Guilherme H <ghpm99@gmail.com>
2023-08-02 02:14:36 -0300

    Include description form

--- apps/video/forms.py
+++ apps/video/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=5000, widget=forms.Textarea)
     file = forms.FileField()
