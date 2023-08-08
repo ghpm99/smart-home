71ebd8475e36bb394f76bd7e2ba6a6d6cf47099e
Guilherme H <ghpm99@gmail.com>
2023-08-08 02:12:16 -0300

    Feat: Implements solare template

--- apps/video/views.py
+++ apps/video/views.py
@@ -35,9 +35,39 @@ def video(request):
         'file_base': video.file_base or '',
     } for video in videos]
 
+    class_options = [
+        {'value': 'Archer', 'label': 'Arqueiro'},
+        {'value': 'Berserker', 'label': 'Berserker'},
+        {'value': 'Witch', 'label': 'Bruxa'},
+        {'value': 'Dark Knight', 'label': 'Cavaleira das Trevas'},
+        {'value': 'Ranger', 'label': 'Caçadora'},
+        {'value': 'Corsair', 'label': 'Corsaria'},
+        {'value': 'Tamer', 'label': 'Domadora'},
+        {'value': 'Drakania', 'label': 'Drakania'},
+        {'value': 'Sorceress', 'label': 'Feiticeira'},
+        {'value': 'Guardian', 'label': 'Guardia'},
+        {'value': 'Warrior', 'label': 'Guerreiro'},
+        {'value': 'Hashashin', 'label': 'Hashashin'},
+        {'value': 'Kunoichi', 'label': 'Kunoichi'},
+        {'value': 'Lahn', 'label': 'Lahn'},
+        {'value': 'Striker', 'label': 'Lutador'},
+        {'value': 'Maehwa', 'label': 'Maehwa'},
+        {'value': 'Wizard', 'label': 'Mago'},
+        {'value': 'Maegu', 'label': 'Me-gu'},
+        {'value': 'Mystic', 'label': 'Mistica'},
+        {'value': 'Musa', 'label': 'Musa'},
+        {'value': 'Ninja', 'label': 'Ninja'},
+        {'value': 'Nova', 'label': 'Nova'},
+        {'value': 'Sage', 'label': 'Sage'},
+        {'value': 'Shai', 'label': 'Shai'},
+        {'value': 'Valkyrie', 'label': 'Valquiria'},
+        {'value': 'Woosa', 'label': 'Wu-sa'},
+    ]
+
     ctx = {
         "form": form,
         "videos": videos_data,
+        "class_options": class_options,
     }
 
-    return render(request, "video.html", ctx)
+    return render(request, "video/video.html", ctx)
