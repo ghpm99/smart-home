33f4488a427896e4847224e25c63ee0741e77d40
Guilherme H <ghpm99@gmail.com>
2023-08-23 17:28:24 -0300

    Feat: implements Video youtube model

--- apps/video/admin.py
+++ apps/video/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from video.models import Video
+from video.models import Video, VideoYoutube
 
 
 # Register your models here.
@@ -8,4 +8,10 @@ class VideoConfig(admin.ModelAdmin):
     pass
 
 
+class VideoYoutubeConfig(admin.ModelAdmin):
+    list_display = ['id', 'youtube_id', 'upload_status', 'privacy_status']
+    pass
+
+
+admin.site.register(VideoYoutube, VideoYoutubeConfig)
 admin.site.register(Video, VideoConfig)
--- apps/video/management/commands/update_video_data.py
+++ apps/video/management/commands/update_video_data.py
@@ -4,9 +4,10 @@ import time
 import httplib2
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 from googleapiclient.discovery import build
 from oauth2client.file import Storage
-from video.models import Video
+from video.models import Video, VideoYoutube
 
 
 class Command(BaseCommand):
@@ -29,18 +30,41 @@ class Command(BaseCommand):
                 http=credentials.authorize(httplib2.Http())
             )
 
-        video = Video.objects.filter(status=Video.S_SUCCESS).last()
-        print(video.youtube_id)
+        videos = Video.objects.filter(status=Video.S_SUCCESS).order_by('id').all()
+
+        videos_paginator = Paginator(videos, 50)
+
+        print(videos_paginator.get_page(1))
+
+        videos_id = []
+
+        for video in videos_paginator.get_page(1):
+            videos_id.append(video.youtube_id)
+
+        separator = ','
+
+        youtube_id_separator = separator.join(videos_id)
 
         youtube = get_authenticated_service()
 
         request = youtube.videos().list(
             part="statistics,status",
-            id=video.youtube_id
+            id=youtube_id_separator
         )
         response = request.execute()
 
-        print(response)
+        for youtube_data in response.get('items'):
+            video_youtube, created = VideoYoutube.objects.get_or_create(
+                youtube_id=youtube_data.get('id'), defaults={'youtube_id': youtube_data.get('id')})
+            video_status = youtube_data.get('status')
+            video_youtube.upload_status = video_status.get('uploadStatus')
+            video_youtube.privacy_status = video_status.get('privacyStatus')
+            video_statistics = youtube_data.get('statistics')
+            video_youtube.view_count = video_statistics.get('viewCount')
+            video_youtube.like_count = video_statistics.get('likeCount')
+            video_youtube.dislike_count = video_statistics.get('dislikeCount')
+            video_youtube.comment_count = video_statistics.get('commentCount')
+            video_youtube.save()
 
     def handle(self, *args, **options):
         begin = time.time()
