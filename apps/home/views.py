22cc8eb5bf13eb65be4c662016af0445d9689d52
Guilherme H <ghpm99@gmail.com>
2023-11-14 22:40:50 -0300

    Feat: update to python 3.12

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
 import platform
+import psutil
+
+from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
     ctx = {
-        'computer_name': platform.node()
+        'computer_name': platform.node(),
+        'cpu_times': psutil.cpu_times(),
+        'cpu_percent': psutil.cpu_percent(),
+        'cpu_status': psutil.cpu_stats(),
+        'virtual_memory': psutil.virtual_memory(),
+        'disk_partitions': psutil.disk_partitions()
     }
-    return render(request, 'home.html', ctx)
+    return render(request, 'home/home.html', ctx)
