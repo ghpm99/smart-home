28947171e56a80a45f58b15525ceb00def997f7a
Guilherme H <ghpm99@gmail.com>
2023-11-15 23:31:43 -0300

    Feat: Implements cards

--- apps/home/views.py
+++ apps/home/views.py
@@ -6,12 +6,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+
     ctx = {
         'computer_name': platform.node(),
         'cpu_times': psutil.cpu_times(),
         'cpu_percent': psutil.cpu_percent(),
         'cpu_status': psutil.cpu_stats(),
         'virtual_memory': psutil.virtual_memory(),
-        'disk_partitions': psutil.disk_partitions()
+        'disk_partitions': [{
+            'device': disk.device,
+            'mountpoint': disk.mountpoint,
+            'fstype': disk.fstype,
+            'opts': disk.opts,
+            'usage': psutil.disk_usage(disk.device)
+        } for disk in psutil.disk_partitions()]
     }
     return render(request, 'home/home.html', ctx)
