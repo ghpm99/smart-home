c97f805d59cb6523e942e24b432df5afc257bf6b
Guilherme H <ghpm99@gmail.com>
2024-01-19 13:12:43 -0300

    Feat: implements copy files to backup

--- apps/home/management/commands/backup.py
+++ apps/home/management/commands/backup.py
@@ -1,13 +1,14 @@
-from datetime import datetime
 import os
-from pathlib import Path
+import shutil
 import time
-import json
+from datetime import datetime
+from pathlib import Path
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
 from home.models import File
+from django.utils import timezone
+from django.db.models import F, Q
 
 
 class Command(BaseCommand):
@@ -18,33 +19,48 @@ class Command(BaseCommand):
     def run_command(self):
 
         print('Iniciando backup de arquivos')
+        backup_start = datetime.now(tz=timezone.utc)
+        meta_data = f'bkp_{backup_start.strftime("%Y%m%d_%H%M%S")}.txt'
+        Path(settings.BACKUP_FOLDER).mkdir(parents=True, exist_ok=True)
 
         for root, dirs, files in os.walk(settings.SHARED_FOLDER):
-
-            dir = root.replace(settings.SHARED_FOLDER, settings.BACKUP_FOLDER)
-            print(dir)
-            Path(dir).mkdir(parents=True, exist_ok=True)
 
             for file in files:
                 new_file, created = File.objects.get_or_create(
                     name=file,
                     path=root,
-                    format=file.split('.')[-1]
+                    defaults={
+                        'format': file.split('.')[-1],
+                        'size': os.path.getsize(f'{root}/{file}'),
+                        'updated_at': datetime.fromtimestamp(os.path.getmtime(f'{root}/{file}'), tz=timezone.utc),
+                        'created_at': datetime.fromtimestamp(os.path.getctime(f'{root}/{file}'), tz=timezone.utc),
+                        'last_interaction': backup_start,
+                    }
                 )
                 if created:
                     print(f'Arquivo {file} criado')
+                else:
+                    new_file.last_interaction = backup_start
+                    new_file.save()
 
-                with open(f'{dir}/{file}.bkp', 'w+', encoding='utf-8') as f:
-                    data = {
-                        'id': new_file.id,
-                        'name': new_file.name,
-                        'path': new_file.path,
-                        'format': new_file.format,
-                        'size': os.path.getsize(f'{root}/{file}'),
-                        'last_modified': datetime.fromtimestamp(os.path.getmtime(f'{root}/{file}')).strftime('%Y-%m-%dT%H:%M:%S'),
-                        'created': datetime.fromtimestamp(os.path.getctime(f'{root}/{file}')).strftime('%Y-%m-%dT%H:%M:%S')
-                    }
-                    json.dump(data, f, ensure_ascii=False, indent=4)
+        files_to_backup = File.objects.filter(Q(last_backup__isnull=True) | Q(last_backup__lte=F('updated_at'))).all()
+
+        print(f'Arquivos para backup: {files_to_backup.count()}')
+        print(f'Lista de arquivos pode ser encontrado em {settings.BACKUP_FOLDER}{meta_data}')
+
+        with open(f'{settings.BACKUP_FOLDER}{meta_data}', 'a+', encoding='utf-8') as f:
+            f.write(f'Arquivos para backup: {files_to_backup.count()}\n')
+            f.write(f'Horario de inicio: {backup_start.strftime("%Y-%m-%d %H:%M:%S")}\n')
+            for file in files_to_backup:
+                f.write(f'{file.path}{file.name}\n')
+            f.close()
+
+        for file_to_backup in files_to_backup:
+            dir = file_to_backup.path.replace(settings.SHARED_FOLDER, settings.BACKUP_FOLDER)
+            Path(dir).mkdir(parents=True, exist_ok=True)
+            target_file = f'{file_to_backup.path}/{file_to_backup.name}'
+            print(f'Copiando {target_file} para {dir}')
+            shutil.copy(target_file, dir)
 
         print('Concluiu')
 
--- apps/home/models.py
+++ apps/home/models.py
@@ -6,5 +6,8 @@ class File(models.Model):
     name = models.CharField(max_length=256)
     path = models.CharField(max_length=512)
     format = models.CharField(max_length=256)
+    size = models.IntegerField()
     updated_at = models.DateTimeField()
     created_at = models.DateTimeField()
+    last_interaction = models.DateTimeField(auto_now=True)
+    last_backup = models.DateTimeField(null=True)
