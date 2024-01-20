376a91be39539ae54dd6f5207ad11078aa650e72
Guilherme H <ghpm99@gmail.com>
2024-01-20 20:15:04 -0300

    Fix: upload video script

--- apps/video/management/commands/upload_video.py
+++ apps/video/management/commands/upload_video.py
@@ -126,7 +126,6 @@ class Command(BaseCommand):
 
             status_video = dict(
                 privacyStatus=privacy,
-                publishAt=video.publish_at.strftime('%Y-%m-%dT%H:%M:%SZ')
             ),
 
             if video.type is Video.T_SOLARE_RANKED or video.type is Video.T_SOLARE_PRACTICE:
