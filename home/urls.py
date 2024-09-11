25653e06c6e9b01ad12b37ec839d53cb871442ef
Guilherme H <ghpm99@gmail.com>
2024-09-10 22:46:17 -0300

    Fix: url

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,11 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('home/', view=views.home, name='home'),
+    path('', view=views.home, name='home'),
     path('media/', include([
-        path('files/', view=views.files, name='media-files'),
-        path('documents/', view=views.documents, name='media-documents'),
-        path('images/', view=views.images, name='media-images'),
-        path('video/', view=views.video, name='media-video')
+        path('files', view=views.files, name='media-files'),
+        path('documents', view=views.documents, name='media-documents'),
+        path('images', view=views.images, name='media-images'),
+        path('video', view=views.video, name='media-video')
     ]))
 ]
